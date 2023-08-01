a230f6fce2cf8b47872b784c80a083e627898c74
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-08-01 10:51:46 -0300

    Create Gerador de senhas.c

---
+++ Gerador de senhas.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{   
+    int a;
+    printf("Qual tamanho da senha que voce deseja?\n");
+    scanf("%d",&a);
+    
+    char letras[] = "abcdefghijklmnopqrstuvwyzx";
+    char numeros[] = "0123456789";
+    
+}
