813f9d6b3f8dd41de6c4a4d3cd8ce3d8bdee93f7
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-08-29 11:21:33 -0300

    Create Exemplo_uso_if.c

---
+++ Exemplo_uso_if.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{   int a = 4;
+    int b = 2;
+    int c = 1;
+    if(a%2==0){
+        printf("Par\n");
+    }
+    if(a>2){
+        printf("Valor maior que 2\n");
+    }
+}
