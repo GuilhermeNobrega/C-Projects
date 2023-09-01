1eb0f4d8a62b2244a95e205417dbac295ea5b6a4
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-08-31 23:48:38 -0300

    Testes com switch

---
+++ Switch_function.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+const char* minhaFuncao() {
+    char frase[] = "Rodou!!";
+    return frase;
+}
+
+int main() {
+    printf("1 - Main\n");
+    int valor = 2;
+
+    switch (valor) {
+        case 1:
+            printf("Caso 1\n");
+            break;
+        case 2:
+            printf("Resultado da funcao: %s\n", minhaFuncao());
+            break;
+        case 3:
+            printf("Caso 3\n");
+            break;
+        default:
+            printf("Caso padrão\n");
+    }
+
+    return 0;
+}
