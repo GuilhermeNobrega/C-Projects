8f80c142e74ac5e818ddbd250dc1879607f4a637
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-09-04 09:12:41 -0300

    Create Função_comParametro.c

---
+++ Função_comParametro.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <string.h>
+int tamanho(char changes[20]){
+    printf("Vamos analisar:\n");
+    int frase = strlen(changes);
+    printf("Tamanho da frase solicitada eh %d\n", frase);
+    }
+int main()
+{   int sla;
+    printf("Vamos tentar executar essa funcao..\n");
+    sla = tamanho("abcd");
+}
