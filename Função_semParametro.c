f76a391c5c6c5fc30d9caf9d9fcbc51546f39ece
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-09-04 09:12:17 -0300

    Update Função_semParametro.c

--- Função_semParametro.c
+++ Função_semParametro.c
@@ -3,10 +3,10 @@
 #include <string.h>
 int tamanho(){
     char changes[20];
-    printf("Digite a frase para ver quntas letras tem:");
+    printf("Digite a frase para ver quntas letras tem:\n");
     scanf("%s",&changes);
     int frase = strlen(changes);
-    printf("Tamanho da frase solicitada eh %d", frase);
+    printf("Tamanho da frase solicitada eh %d\n", frase);
     }
 int main()
 {   int sla;
