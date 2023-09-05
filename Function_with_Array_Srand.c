68d6e42828635c2ce6f78d69141bba554a2d86d4
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-09-05 09:11:24 -0300

    Update Function_with_Array_Srand.c

--- Function_with_Array_Srand.c
+++ Function_with_Array_Srand.c
@@ -25,3 +25,30 @@ int main(){
     }
     printf("Soma dos valores dentro do vetor: = %d",salvar);
 }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int aleatorio(){
+    int valor;
+    printf("Vamos gerar valores aleatorios...\n");
+    //srand (time(NULL));
+    valor = rand()%20+1;
+    return valor;
+}
+
+
+int main(){
+    int c =0;
+    int salvar = 0;
+    int notas;
+    int vetordoido[7];
+    for(c;c<7;c++){
+        vetordoido[c] = aleatorio();
+        printf("Valor do vetor na posicao [%d] eh: %d\n",c,vetordoido[c]);
+        salvar = salvar+vetordoido[c];
+    }
+    printf("Soma dos valores dentro do vetor: = %d",salvar);
+    return 1;
+}
